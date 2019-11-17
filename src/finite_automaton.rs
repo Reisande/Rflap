1afca999935ccb1bba863158b8bdf2c4dfaa089a
Reisande <mryabk2@uic.edu>
2019-11-16 20:32:15 -0600

    Implemented validate/trace string, but have not tested

--- src/finite_automaton.rs
+++ src/finite_automaton.rs
@@ -33,6 +33,10 @@ impl FiniteAutomaton {
 	pub fn insert_char_in_alphabet(&mut self, new_char : char) {
 		self.alphabet.insert(new_char);
 	}
+
+	pub fn delete_char_in_alphabet(&mut self, new_char : char) {
+		self.alphabet.remove(&new_char);
+	}
 	
 	fn change_start(&mut self, new_start : &String) -> bool {
 		let should_change = match self.states.get(&new_start.to_string()) {
@@ -93,7 +97,7 @@ impl FiniteAutomaton {
 		}
 		else {
 			println!("Tried to incorrectly insert {:?}", state_name);
-			println!("{:?} is either a copy of an existing value, or delete failed",
+			println!("{:?} is either a copy of an existing value, or previous delete failed",
 					 state_name);
 		}		
 	}
@@ -132,6 +136,47 @@ impl FiniteAutomaton {
 		}
 	}
 
+	// returns a path of transitions and states, beginning with the start state
+	// and ending with a final state if the automaton accepts the string. If
+	// the automaton rejects the string returns None. Maybe this should
+	// be changed to a Result, though
+
+	// this function assumes that the validation that the string is valid for the
+	// alphabet occurs on the client side
+	fn validate_string(self, validate_string : String) -> Option<Vec<(char, String)>> {
+		if self.determinism {
+			let mut return_vec : Vec<(char, String)> = [('_', self.start_state)].to_vec();
+
+			// iterate through the given string, moving from state to state, until
+			// the string is empty and the end state is reached or not reached
+			for symbol in validate_string.chars() {
+				// the current state is the one at the end of the return vec
+
+				// 	transition_function : HashMap<(String, Option<char>), String>,
+
+				let current_state : String = match return_vec.last() {
+					Some((_, state)) => state.to_string(),
+					None => "".to_string()
+				};
+
+				let next_state : String =
+					match self.transition_function.get(&(current_state, Some(symbol))) {
+						Some(new_state) => new_state.to_string(),
+						None => "".to_string()
+					};
+													   
+				return_vec.push((symbol, next_state));
+			}
+
+			// check to see that the last state pushed is an accepting state
+			
+			Some(return_vec)
+		}
+		else {
+			None
+		}
+	}
+	
 	fn check_determinism(self) -> bool {
 		if self.determinism {
 			for i in self.transition_function.iter() {
@@ -146,8 +191,6 @@ impl FiniteAutomaton {
 	}
 	
 	/*
-	fn validate_string() -> () { }
-
 	fn trace_string() -> () { }
 
 	fn serialize_json() -> () { }
