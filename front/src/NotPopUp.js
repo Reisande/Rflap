cf0b04f2093af563f3a5b6d85fba9f30628cf286
heldeo <heldeo3@hotmail.com>
2020-01-12 18:29:34 -0600

    added import button, created Popups with versatile component to display error messages for importation and non-valid DFA

---
+++ front/src/NotPopUp.js
@@ -0,0 +1,18 @@
+
+import React,{} from 'react';
+
+import Popup from 'reactjs-popup';
+
+
+function NotPopUp(props){
+
+    return(
+        <div>
+            <text>{props.text}</text>
+        </div>
+    );
+
+
+}
+
+export default NotPopUp.js;
