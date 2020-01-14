f79f1c377d664914eeb434232b60eca5850dace1
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2020-01-14 01:01:10 -0600

    cargo cleanup + docker fix

--- temp-api/src/server.ts
+++ temp-api/src/server.ts
@@ -7,6 +7,7 @@ import morgan from 'morgan';
 
 const app: express.Application = express();
 
+const automata_exec = process.env.NODE_ENV === 'production' ? 'automata' : 'cargo run';
 
 app.set('port', process.env.PORT || 8080);
 
@@ -18,7 +19,9 @@ app.use(express.static(path.resolve('../front/build')));
 
 app.post('/api', (req: Request, res: Response) => {
     const json = JSON.stringify(req.body, null);
-    child_process.exec(`echo '${json}' | cargo run automata `, {cwd: '..'}, (error, stdout, stderr) => {
+    console.log(json);
+    console.log(automata_exec);
+    child_process.exec(`echo '${json}' | ${automata_exec} automata`, {cwd: '..'}, (error, stdout, stderr) => {
         if (error) res.status(500).send(stderr);
         else res.status(200).send(stdout);
     });
