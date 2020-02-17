ed9c4818586419a03a0dd68edd0f84aedba41f66
Reisande <42556808+Reisande@users.noreply.github.com>
2020-02-17 00:28:58 -0600

    Backend/equivalence between automata (#63)
    * added all the testing framework which SHOULD wor with the autograder, now I just need to deserialize all relevant objects and test in production type environment
    
    * Implemented reading from file passed in as arg
    
    * Finished the reading and writing to buffer files, but have not tested due to lack of decryption function
    
    * Fixed a few logic errors in the test generation
    
    * Implemented fix to prevent seg faults when reading test cases
    
    * Working and tested version of the equivalence testing. Need to clean up the grade function a bit so that students can see what the expected output of the test was, and what the actual output. Also the return value of grade() should be made into a class object.
    
    * Cleaning up the branch a bit
    
    * Fixing small bug in parsing automata
    
    * resolving merge conflicts
    
    * Cleaning up branch
    
    * basic skeleton of newest autograder format set up in finite_automata, but not in main
    
    * Wrote skeleton code for homework 2 autograder, need to add relevant arguments to the autograder crate
    
    * New setup for hw2 compiling, about to test

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::io::{BufRead, BufReader, Error, Write};
 use std::path::Path;
 
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
+use serde_json::{Result, to_string};
 
 use multimap::MultiMap;
 
@@ -46,9 +46,10 @@ fn tests(tests: generate_tests::TestsJson) -> Result<String> {
 
 // return is a tuple of number of passed test cases vs total cases
 fn grade(
-    source: finite_automaton::FiniteAutomatonJson,
-    target: finite_automaton::FiniteAutomatonJson,
+    source: &finite_automaton::FiniteAutomatonJson,
+    target: &finite_automaton::FiniteAutomatonJson,
     num_tests: u16,
+    supposed_to_be_deterministic : bool,
 ) -> (
     u16,
     u16,
@@ -120,7 +121,7 @@ fn grade(
         deterministic_scores,
         test_strings_nondeterministic,
         nondeterministic_scores,
-        source.is_deterministic() || !target.is_deterministic(), // count as 5/15 for false
+        !supposed_to_be_deterministic || target.is_deterministic(),
     )
 }
 
@@ -150,18 +151,24 @@ fn main() -> io::Result<()> {
         // answer file will be passed in the second command line argument
         let buffer_answer = fs::read_to_string(&args[2])?;
 
+        // TOOD: add config file reader for the grading settings, this is getting bloated
+        let determinism_weight: Option<f64> = Option::from(args[7].to_string().parse::<f64>().unwrap());
+        let supposed_to_be_deterministic = determinism_weight != None;
         // for the actual grading, we should show like 20 shorter strings and hide 80,
-        let public_tests = grade(
-            serde_json::de::from_str::<finite_automaton::FiniteAutomatonJson>(&buffer).unwrap(),
-            serde_json::de::from_str::<finite_automaton::FiniteAutomatonJson>(&buffer_answer)
-                .unwrap(),
+        let source = &serde_json::de::from_str::<finite_automaton::FiniteAutomatonJson>(&buffer).unwrap();
+        let target = &serde_json::de::from_str::<finite_automaton::FiniteAutomatonJson>(&buffer_answer)
+            .unwrap();
+        let mut public_tests = grade(
+            source,
+            target,
             10,
+            supposed_to_be_deterministic,
         );
         let hidden_tests = grade(
-            serde_json::de::from_str::<finite_automaton::FiniteAutomatonJson>(&buffer).unwrap(),
-            serde_json::de::from_str::<finite_automaton::FiniteAutomatonJson>(&buffer_answer)
-                .unwrap(),
+            source,
+            target,
             90,
+            supposed_to_be_deterministic,
         );
 
         // then initialize a data structure which follows the output of results.json
@@ -171,14 +178,40 @@ fn main() -> io::Result<()> {
 
         let problem_number: String = args[4].to_owned();
 
-        let is_properly_deterministic = if public_tests.6 { 0.0 } else { -5.0 };
+        // STATIC TESTS
 
-        tests.push(Tests {
-            score: is_properly_deterministic,
-            name: "determinism".to_string(),
-            number: problem_number.to_owned(),
-            visibility: "visible".to_string(),
-        });
+        // size of the automata in number of states, added as a test to public_tests
+        let original_size: Option<u8> = Option::from(args[5].to_string().parse::<u8>().unwrap());
+        let minimal_size = target.states.len();
+        let size_weight: f64 = f64::from(args[6].to_string().parse::<f64>().unwrap());
+
+        // determinism
+        if public_tests.6 {
+            tests.push(Tests {
+                score: - determinism_weight.unwrap(),
+                name: "determinism".to_string(),
+                number: problem_number.to_owned(),
+                visibility: "visible".to_string(),
+            });
+        }
+        // minimal size
+        match original_size {
+            Some(v) => {
+                let size_score =
+                    - (size_weight *
+                    ((source.states.len() - target.states.len()) /
+                        (v as usize - target.states.len())) as f64);
+                tests.push(Tests{
+                    score: size_score,
+                    name: "size".to_string(),
+                    number: problem_number.to_owned(),
+                    visibility: "visible".to_string(),
+                });
+            }
+            _ => {}
+        }
+
+        // DYNAMIC TESTS
 
         for test in 0..public_tests.2.len() {
             tests.push(Tests {
