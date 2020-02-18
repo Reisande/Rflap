2100495d10f52491c1749f9e9decaeafeae24185
heldeo <heldeo3@hotmail.com>
2020-02-18 17:39:51 -0600

    added menu, context re-render for CFGs and NFAs<->DFA

---
+++ front/src/CFG_Visual.js
@@ -0,0 +1,18 @@
+import React, { useEffect,useState,useRef, useCallback } from 'react';
+import {AutomataContext} from './AutomataContext.js';
+
+
+
+
+function CFG_Visual(){
+
+    return(
+        <div>
+
+        </div>
+    );
+
+};
+
+
+export default CFG_Visual;
