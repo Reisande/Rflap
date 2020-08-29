dd907fbd3b103f42e1138cce78b58237470078a8
heldeo <heldeo3@hotmail.com>
2020-08-29 10:39:11 -0500

    null checks on performance measurement func

--- front/src/Run.js
+++ front/src/Run.js
@@ -469,8 +469,8 @@ const animateIntoNeutral = (status_ref,test_button_ref) =>{
     let dotnet_endpoint = "https://metricsrflap.azurewebsites.net/api/Test/";
     let endpoint = master_context.PDA ? "pda" : "automata";
     //  for testing:
-    //let url = "https://rflap.acmuic.app/" + endpoint;
-    let url = `${window.location.origin}/` + endpoint;
+    let url = "https://rflap.acmuic.app/" + endpoint;
+    //let url = `${window.location.origin}/` + endpoint;
 
     if (packet_to_misha_the_microsoft_engineer.PDA) {
       delete packet_to_misha_the_microsoft_engineer.state_names;
